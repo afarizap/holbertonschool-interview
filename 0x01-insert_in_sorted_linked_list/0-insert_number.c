6a9e56c3ca7111c89e89d4539efd92e2dc31327f
afarizap <afarizap@gmail.com>
2021-04-30 11:29:15 -0500

    updated with creation of malloc

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* insert_node - inserts a number into a sorted sll
+* @head: pointer to head of list
+* @number: number to insert on list
+* Return: the address of the new node, or NULL if it failed
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new;
+    listint_t *current;
+
+    current = *head;
+
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = n;
+    new->next = NULL;
+
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        while (current->next != NULL)
+            current = current->next;
+        current->next = new;
+    }
+
+    return (new);
+}
