bae76421c393420b09db130fc9e4e717c6ed1250
afarizap <afarizap@gmail.com>
2021-05-05 10:40:33 -0500

    fix spaces

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -11,9 +11,9 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-   binary_tree_t *my_node = malloc(sizeof(binary_tree_t));
+    binary_tree_t *my_node = malloc(sizeof(binary_tree_t));
 
-	if (my_node == NULL)
+    if (my_node == NULL)
 		return (NULL);
 	my_node->n = value;
 	my_node->left = NULL;
