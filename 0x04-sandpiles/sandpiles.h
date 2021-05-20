3b7624d5516a663ea01edf8b986f045f59786063
afarizap <afarizap@gmail.com>
2021-05-20 17:25:52 -0500

    add another main and header

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+static void print_grid(int grid[3][3]);
+
+
+#endif /* SANDPILES_H */
