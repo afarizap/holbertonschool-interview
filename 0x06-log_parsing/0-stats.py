cb31e291dd2f853328c80206627afbc4f9a2d57b
afarizap <afarizap@gmail.com>
2021-05-31 22:53:08 -0500

    log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+""" reads stdin line by line and computes metrics """
+
+
+import sys
+from shlex import split
+
+
+calls = 0
+size = 0
+stats = {}
+try:
+    for line in sys.stdin:
+        n = line.split()
+        calls += 1
+        size += int(n[8])
+        if n[7] in stats.keys():
+            tmp = int(stats[n[7]]) + 1
+            stats.update({n[7]: tmp})
+        else:
+            stats.update({n[7]: 1})
+        if calls % 10 == 0:
+            print("File size: {}".format(size))
+            for k, v in sorted(stats.items()):
+                print("{}: {}".format(k, v))
+except KeyboardInterrupt:
+    print("File size: {}".format(size))
+    for k, v in sorted(stats.items()):
+        print("{}: {}".format(k, v))
+    raise
