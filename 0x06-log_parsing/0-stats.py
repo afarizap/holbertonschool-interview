6c494d1bba2bfdddc7788f85d00f1d7ec5416f6a
afarizap <afarizap@gmail.com>
2021-09-01 21:33:32 -0500

    asd

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -10,18 +10,17 @@ stats = {}
 try:
     for line in sys.stdin:
         n = line.split()
-        if len(n) > 2:
-            calls += 1
-            size += int(n[8])
-            if n[7] in stats.keys():
-                tmp = int(stats[n[7]]) + 1
-                stats.update({n[7]: tmp})
-            else:
-                stats.update({n[7]: 1})
-            if calls % 10 == 0:
-                print("File size: {}".format(size))
-                for k, v in sorted(stats.items()):
-                    print("{}: {}".format(k, v))
+        calls += 1
+        size += int(n[8])
+        if n[7] in stats.keys():
+            tmp = int(stats[n[7]]) + 1
+            stats.update({n[7]: tmp})
+        else:
+            stats.update({n[7]: 1})
+        if calls % 10 == 0:
+            print("File size: {}".format(size))
+            for k, v in sorted(stats.items()):
+                print("{}: {}".format(k, v))
 except KeyboardInterrupt:
     pass
 finally:
