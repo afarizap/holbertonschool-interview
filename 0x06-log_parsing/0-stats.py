cd32bff851c05efd1f2152d5399ba8d7f19f0cd8
afarizap <afarizap@gmail.com>
2021-09-05 23:41:51 -0500

    asd

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -28,7 +28,3 @@ try:
                 print("{}: {}".format(k, v))
 except KeyboardInterrupt:
     pass
-finally:
-    print("File size: {}".format(size))
-    for k, v in sorted(stats.items()):
-        print("{}: {}".format(k, v))
