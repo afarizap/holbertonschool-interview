a3bc2479ce87668eb80bb0e75de35dfc01cf42c9
afarizap <afarizap@gmail.com>
2021-09-06 12:05:30 -0500

    add tortoise and hare

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: integer
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise, *hare;
+
+	if (!list || !list->next)
+		return (0);
+	tortoise = list;
+	hare = list->next;
+
+	while (hare->next)
+	{
+		if (tortoise == hare)
+			return (1);
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+	}
+	return (0);
+}
