71ae199039710b4f4ecf5d53fd6aff289a9b4a72
afarizap <afarizap@gmail.com>
2021-09-05 23:34:56 -0500

    add exepts

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,17 +11,21 @@ try:
     for line in sys.stdin:
         n = line.split()
         calls += 1
-        if n[8]:
-            size += int(n[8])
-            if n[7] in stats.keys():
-                tmp = int(stats[n[7]]) + 1
-                stats.update({n[7]: tmp})
-            else:
-                stats.update({n[7]: 1})
-            if calls % 10 == 0:
-                print("File size: {}".format(size))
-                for k, v in sorted(stats.items()):
-                    print("{}: {}".format(k, v))
+        try:
+            int(n[8])
+            int(n[7])
+        except (ValueError, IndexError):
+            continue
+        size += int(n[8])
+        if n[7] in stats.keys():
+            tmp = int(stats[n[7]]) + 1
+            stats.update({n[7]: tmp})
+        else:
+            stats.update({n[7]: 1})
+        if calls % 10 == 0:
+            print("File size: {}".format(size))
+            for k, v in sorted(stats.items()):
+                print("{}: {}".format(k, v))
 except KeyboardInterrupt:
     pass
 finally:
