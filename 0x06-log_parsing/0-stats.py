7149d542f9b87ddedf8143b79be8c4f255714456
afarizap <afarizap@gmail.com>
2021-09-05 23:44:38 -0500

    fix more indexs

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -16,12 +16,12 @@ try:
             int(n[-2])
         except (ValueError, IndexError):
             continue
-        size += int(n[8])
+        size += int(n[-1])
         if n[7] in stats.keys():
             tmp = int(stats[n[-2]]) + 1
             stats.update({n[-2]: tmp})
         else:
-            stats.update({n[7]: 1})
+            stats.update({n[-2]: 1})
         if calls % 10 == 0:
             print("File size: {}".format(size))
             for k, v in sorted(stats.items()):
