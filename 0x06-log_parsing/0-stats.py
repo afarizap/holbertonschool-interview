d611297cc6274a74db75c2b98e0958c964263239
afarizap <afarizap@gmail.com>
2021-09-05 23:27:28 -0500

    fix int

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -11,16 +11,19 @@ try:
     for line in sys.stdin:
         n = line.split()
         calls += 1
-        size += int(n[8])
-        if n[7] in stats.keys():
-            tmp = int(stats[n[7]]) + 1
-            stats.update({n[7]: tmp})
-        else:
-            stats.update({n[7]: 1})
-        if calls % 10 == 0:
-            print("File size: {}".format(size))
-            for k, v in sorted(stats.items()):
-                print("{}: {}".format(k, v))
+        try:
+            size += int(n[8])
+            if n[7] in stats.keys():
+                tmp = int(stats[n[7]]) + 1
+                stats.update({n[7]: tmp})
+            else:
+                stats.update({n[7]: 1})
+            if calls % 10 == 0:
+                print("File size: {}".format(size))
+                for k, v in sorted(stats.items()):
+                    print("{}: {}".format(k, v))
+        except e:
+            pass
 except KeyboardInterrupt:
     pass
 finally:
