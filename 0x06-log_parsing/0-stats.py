df348f3c2e5e536dbddc823ddcf9aa1dc2006cef
afarizap <afarizap@gmail.com>
2021-09-05 23:44:09 -0500

    fix index

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -12,14 +12,14 @@ try:
         n = line.split()
         calls += 1
         try:
-            int(n[8])
-            int(n[7])
+            int(n[-1])
+            int(n[-2])
         except (ValueError, IndexError):
             continue
         size += int(n[8])
         if n[7] in stats.keys():
-            tmp = int(stats[n[7]]) + 1
-            stats.update({n[7]: tmp})
+            tmp = int(stats[n[-2]]) + 1
+            stats.update({n[-2]: tmp})
         else:
             stats.update({n[7]: 1})
         if calls % 10 == 0:
@@ -28,3 +28,7 @@ try:
                 print("{}: {}".format(k, v))
 except KeyboardInterrupt:
     pass
+finally:
+    print("File size: {}".format(size))
+    for k, v in sorted(stats.items()):
+        print("{}: {}".format(k, v))
